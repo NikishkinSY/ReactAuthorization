6a3f56e3b162a90a994e754678318e62ea6d8ab9
Sergey Nikishkin <sergey@gohopscotch.com>
2019-03-07 04:06:05 +0300

    update home controller

--- back-end/Controllers/HomeController.cs
+++ back-end/Controllers/HomeController.cs
@@ -7,12 +7,14 @@ namespace WebApi.Controllers
     [Route("[controller]")]
     public class HomeController : Controller
     {
+        [HttpGet("public")]
         public IActionResult Public()
         {
             return Content("Public");
         }
 
         [Authorize]
+        [HttpGet("private")]
         public IActionResult Private()
         {
             return Content("Private");
